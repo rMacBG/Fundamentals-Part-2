14838e9d831802a1e175e40f283bb33d13222b78
Владимир Василев <95617458+rMacBG@users.noreply.github.com>
2023-11-23 20:57:54 +0200

    exercises

---
+++ MinerTask/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace MinerTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var Resources = new Dictionary<string, uint>();
+
+            string resource;
+            while ((resource = Console.ReadLine()) != "stop")
+            {
+
+                
+                if (!Resources.ContainsKey(resource))
+                {
+                    Resources.Add(resource, 0);
+                }
+                var quantity = uint.Parse(Console.ReadLine());
+                Resources[resource] += quantity;
+                
+            }
+
+            foreach (var item in Resources)
+            {
+                string res = item.Key;
+                uint qty = item.Value;
+                Console.WriteLine($"{res} -> {qty}");
+            }
+        }
+    }
+}
