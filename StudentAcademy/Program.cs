d4e1f0d533e04ed59630185efbc672d12c1260b5
Владимир Василев <95617458+rMacBG@users.noreply.github.com>
2023-11-24 18:22:51 +0200

    st academy task

---
+++ StudentAcademy/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace StudentAcademy
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            var students = new Dictionary<string, Student>();
+            int studentCount = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < studentCount; i++)
+            {
+                string studentName = Console.ReadLine();
+                decimal grade = decimal.Parse(Console.ReadLine());
+
+                if (!students.ContainsKey(studentName))
+                {
+                    students.Add(studentName, new Student(studentName));
+                }
+
+                students[studentName].Grades.Add(grade);
+            }
+            var filteredStudents = students
+                .Where(s => s.Value.Grades
+                .Average() >= 4.50m);
+            foreach (var student in filteredStudents)
+            {
+                Console.WriteLine($"{student.Value}");
+            }
+        }
+    }
+    class Student
+    {
+        public Student(string name)
+        {
+            Name = name;
+            Grades = new List<decimal>();
+        }
+
+        public string Name { get; set; }
+        public List<decimal> Grades { get; set; }
+        public override string ToString()
+        {
+            return $"{Name} -> {Grades.Average():F2}";
+        }
+    }
+}
