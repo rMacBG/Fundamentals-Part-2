44b290a1c6f7d903bda9482f79d1b100acec8534
Владимир Василев <95617458+rMacBG@users.noreply.github.com>
2023-11-29 20:41:06 +0200

    tasks

--- ExtractFile/Program.cs
+++ ExtractFile/Program.cs
@@ -4,7 +4,26 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+           string filePath = Console.ReadLine();
+            
+            string fileName = string.Empty;
+            string fileExtension = string.Empty;
+
+            int separator = filePath.LastIndexOf('\\');
+            int extensionIndex = filePath.LastIndexOf('.');
+
+            if (separator != -1 &&
+                extensionIndex != -1 &&
+                extensionIndex > separator)
+            {
+                fileName = filePath.Substring(separator + 1, 
+                    extensionIndex - separator - 1);
+                fileExtension = filePath.Substring(extensionIndex + 1);
+                
+            }
+
+            Console.WriteLine($"File name: {fileName}");
+            Console.WriteLine($"File extension: {fileExtension}");
         }
     }
 }
---
+++ MultiplyBigNumber/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Numerics;
+
+namespace MultiplyBigNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string number = Console.ReadLine();
+            string multiplier = Console.ReadLine();
+
+            Console.WriteLine(Multiply(number, multiplier));
+        }
+
+        private static string Multiply(string number, string multiplier)
+        {
+            if (number == "0" || multiplier == "0")
+            {
+                return "0";
+            }
+            int carry = 0;
+            int numberMultiplier = int.Parse(multiplier);
+
+            char[] result = new char[number.Length + 1];
+
+            for (int i = number.Length - 1; i >= 0; i--)
+            {
+                int digit = int.Parse(number[i].ToString());
+                int product = digit * numberMultiplier + carry;
+
+                result[i + 1] = (char)(product % 10 + '0');
+                carry = product / 10;
+            }
+
+            if (carry > 0)
+            {
+                result[0] = (char)(carry + '0');
+            }
+
+            return new string(result).TrimStart('\0');
+        }
+    }
+}
