941919e055c62c0ec76f3b5af5f4a69357800eae
Владимир Василев <95617458+rMacBG@users.noreply.github.com>
2023-11-29 21:28:45 +0200

    finalTasks and folder for RegEx

---
+++ StringExplosion/Program.cs
@@ -0,0 +1,43 @@
+﻿/*
+ peter>2sis>1a>2akarate>4hexmaster
+ */
+
+using System.Text;
+
+namespace StringExplosion
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string input = Console.ReadLine();
+
+            string result = Explosions(input);
+            Console.WriteLine(result);
+        }
+
+        private static string Explosions(string input)
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            int strength = 0;
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] == '>')
+                {
+                    strength += int.Parse(input[i + 1].ToString());
+                    stringBuilder.Append(input[i]);
+                }
+                else if (strength == 0)
+                {
+                    stringBuilder.Append(input[i]);
+                }
+                else
+                {
+                    strength--;
+                }
+
+            }
+            return stringBuilder.ToString();
+        }
+    }
+}
