676d5b4899233603c4cf2004281dd6f79f114a6f
Владимир Василев <95617458+rMacBG@users.noreply.github.com>
2023-11-29 20:53:12 +0200

    more tasks

---
+++ ReplaceRepeatingStrings/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Text;
+
+namespace ReplaceRepeatingStrings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+
+            if (string.IsNullOrEmpty(str))
+            {
+                Console.WriteLine();
+            }
+
+            StringBuilder result = new StringBuilder();
+
+            result.Append(str[0]);
+            for (int i = 1; i < str.Length; i++)
+            {
+                if (str[i] != str[i-1])
+                {
+                    result.Append(str[i]);
+                }
+                
+            }
+
+            Console.WriteLine(result);
+
+
+        }   
+    }
+}
